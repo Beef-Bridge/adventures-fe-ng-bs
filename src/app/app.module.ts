6755a80ad25e6756d697ed428c9ecceac1ec7f3e
Nikoquattro <nikoquattro@gmail.com>
2022-11-19 23:48:23 +0100

    install et use fontawesome (npm install @fortawesome/fontawesome-svg-core npm install @fortawesome/free-brands-svg-icons npm install @fortawesome/free-regular-svg-icons npm install @fortawesome/free-solid-svg-icons npm install @fortawesome/angular-fontawesome)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import {
+  faPersonHiking,
+  faMountain,
+  faArrowTrendUp,
+  faRoute,
+} from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule, AppRoutingModule],
+  imports: [BrowserModule, NgbModule, FontAwesomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Add packs Font-Awesome
+    library.addIconPacks(fab);
+    library.addIconPacks(far);
+    library.addIconPacks(fas);
+  }
+}
