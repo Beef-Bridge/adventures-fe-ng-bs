9b882e26d6ae99a9cffc456c57f2766279b0915b
Nikoquattro <nikoquattro@gmail.com>
2023-11-07 22:05:21 +0100

    creation sidebar homepage

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageSidebarComponent } from './homepage-sidebar/homepage-sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    HomepageSidebarComponent,
   ],
   imports: [
     CommonModule
---
+++ src/app/home/homepage-sidebar/homepage-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-sidebar',
+  templateUrl: './homepage-sidebar.component.html',
+  styleUrls: ['./homepage-sidebar.component.scss']
+})
+export class HomepageSidebarComponent {
+
+}
