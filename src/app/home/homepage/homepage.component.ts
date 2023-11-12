c3ed22a36a6ab213388443efb6e88120a7b3188f
Nikoquattro <nikoquattro@gmail.com>
2023-11-12 22:46:23 +0100

    feat [home]: récupération liste randonnées

--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMountain, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { Hike } from 'src/app/shared/models/hike';
+import { HikeService } from 'src/app/shared/services/hike.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  // icones FontAwesome
   faMountain = faMountain;
   faLocationDot = faLocationDot;
+
+  public hikeList: Hike[] = [];
+
+  constructor(private _hikeService: HikeService) {}
+
+  ngOnInit(): void {
+    this._hikeService.list$().subscribe((hikes: Hike[]) => {
+      console.table(hikes);
+      this.hikeList = hikes;
+    });
+  }
 }
