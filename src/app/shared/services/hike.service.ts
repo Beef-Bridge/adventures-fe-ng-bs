b62eedbbdd77f0a8dbccb7c3d9cc3b0187e295df
Nikoquattro <nikoquattro@gmail.com>
2023-11-13 21:19:35 +0100

    feat [common]: mise en place fichiers environnements et utilisation

--- src/app/shared/services/hike.service.ts
+++ src/app/shared/services/hike.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hike } from '../models/hike';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +11,13 @@ export class HikeService {
   constructor(private _http: HttpClient) {}
 
   public list$ = (): Observable<Hike[]> => {
-    // URL de la ressource vers l'API "hikes" (fichier api/db.json)
-    const URL_API: string = 'http://localhost:3001/hikes';
-
-    return this._http
-      .get<Hike[]>(URL_API)
+    let hikeList = this._http
+      .get<Hike[]>(`${environment.urlApi}/hikes`)
       .pipe(map((data: Hike[]) => data.map((hike) => new Hike(hike))));
+
+    // @todo: regrouper les randos par années et formatter le jours et mois avec DayJs comme sur la maquette !
+    console.log('liste rando hikeService : ', hikeList);
+
+    return hikeList;
   };
 }
