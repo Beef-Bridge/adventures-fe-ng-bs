23cf5b17811ae1e645444cdfeaf1ebef53b6d08a
Nikoquattro <nikoquattro@gmail.com>
2023-11-20 21:15:58 +0100

    feat [hike]: déclaration des composants hike dans le hike.module

--- src/app/hike/hike.module.ts
+++ src/app/hike/hike.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HikeRoutingModule } from './hike-routing.module';
+import { ListHikeComponent } from './list-hike/list-hike.component';
+import { AddHikeComponent } from './add-hike/add-hike.component';
+import { EditHikeComponent } from './edit-hike/edit-hike.component';
+import { DetailHikeComponent } from './detail-hike/detail-hike.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListHikeComponent,
+    DetailHikeComponent,
+    AddHikeComponent,
+    EditHikeComponent,
+  ],
   imports: [CommonModule, HikeRoutingModule],
 })
 export class HikeModule {}
