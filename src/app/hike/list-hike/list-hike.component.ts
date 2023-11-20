dfeb317b04eda7e87906e2c5b1216d5e226c244c
Nikoquattro <nikoquattro@gmail.com>
2023-11-20 22:20:46 +0100

    feat [hike]: liste des randonnées

--- src/app/hike/list-hike/list-hike.component.ts
+++ src/app/hike/list-hike/list-hike.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Hike } from 'src/app/shared/models/hike';
+import { HikeService } from 'src/app/shared/services/hike.service';
 
 @Component({
   selector: 'app-list-hike',
   templateUrl: './list-hike.component.html',
-  styleUrls: ['./list-hike.component.scss']
+  styleUrls: ['./list-hike.component.scss'],
 })
-export class ListHikeComponent {
+export class ListHikeComponent implements OnInit {
+  public hikesHeaders = ['Nom', 'Massif', 'type', 'Date'];
+  hikeList$!: Observable<Hike[]>;
 
+  constructor(private _router: Router, private _hikeService: HikeService) {}
+
+  public ngOnInit(): void {
+    this.hikeList$ = this._hikeService.sortHikeListByDateDesc();
+  }
 }
