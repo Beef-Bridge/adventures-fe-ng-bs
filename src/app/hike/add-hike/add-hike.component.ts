f0d860496d6be2b62e4f5bbe1c3e555743f85069
Nikoquattro <nikoquattro@gmail.com>
2023-11-20 21:14:49 +0100

    feat [hike]: creation des composants add,detail,edit,list

---
+++ src/app/hike/add-hike/add-hike.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-hike',
+  templateUrl: './add-hike.component.html',
+  styleUrls: ['./add-hike.component.scss']
+})
+export class AddHikeComponent {
+
+}
---
+++ src/app/hike/list-hike/list-hike.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-hike',
+  templateUrl: './list-hike.component.html',
+  styleUrls: ['./list-hike.component.scss']
+})
+export class ListHikeComponent {
+
+}
